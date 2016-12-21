307c2a4d67cf606158b9dd121c3b87c006bf3b0e
antoinelanco <antoinelanco@gmail.com>
2016-12-21 20:46:06 +0000

    git.sh

--- git.sh
+++ git.sh
@@ -1,3 +1,3 @@
 git add *
 git commit -m $1
-git push http://antoinelanco:datalife02@git.com/antoinelanco/Patator --all
+git push http://antoinelanco:datalife02@github.com/antoinelanco/Patator --all
