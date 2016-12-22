8027ce65e7e7b0f9393574c40e972b9afacd3ccf
antoinelanco <antoinelanco@gmail.com>
2016-12-22 13:57:37 +0000

    test

---
+++ test.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(16, GPIO.OUT)
+try:
+
+    GPIO.output(16, GPIO.HIGH)
+    time.sleep(0.1)
+    GPIO.output(16, GPIO.LOW)
+    GPIO.cleanup()
+except KeyboardInterrupt:
+    GPIO.cleanup()
